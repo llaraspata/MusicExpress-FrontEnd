ca731cfd96b1a606eb1e9178dd6679cf93b5838c
Ivan Rinaldi <ivanrinal20@gmail.com>
2024-01-03 16:16:16 +0100

    Updated API urls

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -20,9 +20,13 @@ export class DashboardComponent {
   @ViewChild("trainPlaylist", { static: false }) trainPlaylist?: ElementRef;
   @ViewChild("testPlaylist", { static: false }) testPlaylist?: ElementRef;
 
-  private getAvailablePlaylistsUrl = 'http://musicexpress.azurewebsites.net:8000/available_playlists';
-  private postRecommendedSongsUrl = 'http://musicexpress.azurewebsites.net:8000/recommendation';
-  private postExtractPlaylistUrl = 'http://musicexpress.azurewebsites.net:8000/extract';
+  /*private getAvailablePlaylistsUrl = 'https://musicexpress.azurewebsites.net:8000/available_playlists';
+  private postRecommendedSongsUrl = 'https://musicexpress.azurewebsites.net:8000/recommendation';
+  private postExtractPlaylistUrl = 'https://musicexpress.azurewebsites.net:8000/extract';*/
+
+  private getAvailablePlaylistsUrl = 'https://stagingmusicexpress.azurewebsites.net:8000/available_playlists';
+  private postRecommendedSongsUrl = 'https://stagingmusicexpress.azurewebsites.net:8000/recommendation';
+  private postExtractPlaylistUrl = 'https://stagingmusicexpress.azurewebsites.net:8000/extract';
 
   availablePlaylists: Playlist[] = [];
 
